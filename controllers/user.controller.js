24c01cab5416c3e790f9e02e2992fef4dc3a27c5
binod rai <br163406@gmail.com>
2026-01-21 20:35:21 +0545

    created auth middleware which accept jwt token and give permission or reject them

---
+++ controllers/user.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/user.model.js";
+
+//Get All Users
+export const getUsers = async (req, res, next) => {
+   try {
+      const users = await User.find().select("-password");
+
+      res.status(200).json({
+         sucess: true,
+         message: "all users fetch sucessfull",
+         data: users,
+      });
+   } catch (error) {
+      next(error);
+   }
+};
+
+//Get single User
+export const getUser = async (req, res, next) => {
+   try {
+      const user = await User.findById(req.params.id).select("-password");
+
+      if (!user) {
+         const error = new Error("user doesn't exist");
+         error.statusCode = 404;
+         throw error;
+      }
+
+      res.status(200).json({
+         sucess: true,
+         data: user,
+      });
+   } catch (error) {
+      next(error);
+   }
+};
